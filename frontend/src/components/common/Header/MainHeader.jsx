09eef69a241dadd4298d4749f099a60a21632062
Amna BB <theamnabb@gmail.com>
2025-06-20 17:31:05 +0500

    added

--- frontend/src/components/common/Header/MainHeader.jsx
+++ frontend/src/components/common/Header/MainHeader.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 import Navbar from "./Navbar";
 const MainHeader = () => {
   return (
-    <header>
-      <div>
+    <header className="mx-auto max-w-[1440px] px-6 lg:px-12 w-full absolute top-0 left-0 right-0 z-50 bg-deep text-white py-3">
+      <div className="flex items-center justify-between; ">
         {/* Logos */}
-        <Link>
-          <span className="text-tertiary ">
-            <span className="" >T</span>
+        <Link to={"/"} className="text-[24px] font-bold leading-[120%] flex-1 flex">
+          <span className="inline-flex" >
+            <span className="inline-flex items-center justify-center p-2 h-8 w-8 bg-secondary text-tertiary rotate-[-31deg] rounded-full" >T</span>
             utorFinder
           </span>
         </Link>
