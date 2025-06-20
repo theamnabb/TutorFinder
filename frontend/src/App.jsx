1e21dfee9720fd597f0436ff5e2db08882961063
Amna BB <theamnabb@gmail.com>
2025-06-20 12:07:37 +0500

    <App />

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
+import Header from "./components/Header";
 
 function App() {
-  
-
   return (
     <>
       <h1 className="font-bold text-3xl text-center text-yellow-500">Helo</h1>
+      <Header />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
