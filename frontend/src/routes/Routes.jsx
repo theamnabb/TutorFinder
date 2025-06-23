9bd16270b70f3b87172d3c5211e2189547264e47
Amna BB <theamnabb@gmail.com>
2025-06-23 19:17:23 +0500

    sessions Complete

--- frontend/src/routes/Routes.jsx
+++ frontend/src/routes/Routes.jsx
@@ -1,32 +1,74 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from '../pages/Home';
-import Tutors from '../pages/Tutors';   
-import Contact from '../pages/Contact';
-import Blog from '../pages/Blog';
-import MyProfile from '../pages/MyProfile';
-import MySessions from '../pages/MySessions';
-import Session from '../pages/Session';
-import Verify from '../pages/Verify';
-import Login from '../pages/auth/Login';
-import Layout from '../components/common/Layout';
+import { Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import Tutors from "../pages/Tutors";
+import Contact from "../pages/Contact";
+import Blog from "../pages/Blog";
+import MyProfile from "../pages/MyProfile";
+import MySessions from "../pages/MySessions";
+import Session from "../pages/Session";
+import Verify from "../pages/Verify";
+import Login from "../pages/auth/Login";
+import Layout from "../components/common/Layout";
 const AppRoutes = () => {
   return (
     <>
-    <Routes>
-        <Route path="/" element = {<Layout showFooter showHeader><Home /></Layout>} />
-        <Route path="/tutors" element={<Layout showFooter showHeader><Tutors /></Layout>} />
-        <Route path="/contact" element={<Layout showFooter showHeader><Contact /></Layout>} />
-        <Route path="/blog" element={<Layout showFooter showHeader><Blog/></Layout>} />
-        <Route path="/tutors/:subject" element={<Layout showFooter showHeader><Tutors /></Layout>} />
-        <Route path="/my-profile" element={<MyProfile/>} />
-        <Route path="/my-sessions" element={<MySessions/>} />
-        <Route path="/session/:tutId" element={<Session/>} />
-        <Route path="/verify" element={<Verify/>} />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Layout showFooter showHeader>
+              <Home />
+            </Layout>
+          }
+        />
+        <Route
+          path="/tutors"
+          element={
+            <Layout showFooter showHeader>
+              <Tutors />
+            </Layout>
+          }
+        />
+        <Route
+          path="/contact"
+          element={
+            <Layout showFooter showHeader>
+              <Contact />
+            </Layout>
+          }
+        />
+        <Route
+          path="/blog"
+          element={
+            <Layout showFooter showHeader>
+              <Blog />
+            </Layout>
+          }
+        />
+        <Route
+          path="/tutors/:subject"
+          element={
+            <Layout showFooter showHeader>
+              <Tutors />
+            </Layout>
+          }
+        />
+        <Route path="/my-profile" element={<MyProfile />} />
+        <Route path="/my-sessions" element={<MySessions />} />
+        <Route
+          path="/session/:tutId"
+          element={
+            <Layout showFooter showHeader>
+              <Session />
+            </Layout>
+          }
+        />
+
+        <Route path="/verify" element={<Verify />} />
         <Route path="/login" element={<Login />} />
       </Routes>
-    
     </>
-  )
-}
+  );
+};
 
-export default AppRoutes
+export default AppRoutes;
