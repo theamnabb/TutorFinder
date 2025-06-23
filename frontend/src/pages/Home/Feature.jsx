9255a02cd6d92c1b235359ae3999834cc0dce3db
Amna BB <theamnabb@gmail.com>
2025-06-23 14:19:09 +0500

    fearue

--- frontend/src/pages/Home/Feature.jsx
+++ frontend/src/pages/Home/Feature.jsx
@@ -14,7 +14,7 @@ const FeatureCard = ({ title, icon: Icon, className = "" }) => {
 
 const Feature = () => {
   return (
-    <section className="relative z-20 mx-auto max-w-[1140px] px-6 lg:px-12 bottom-12">
+    <section className="relative z-20 mx-auto max-w-[1140px] px-6 lg:px-12 bottom-5 md:12">
       <div className="flex flex-wrap gap-x-4 bg-white rounded-3xl p-8">
         <FeatureCard title="Qualified Instructors" icon={User} />
         <FeatureCard title="Flexible Scheduling" icon={Clock} className="bg-secondary " />
