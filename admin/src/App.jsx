03b4ddeb9038b81d3ee9cf3c2475e5ffa3f25319
Amna BB <theamnabb@gmail.com>
2025-06-25 14:40:38 +0500

    react dom

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
 
 function App() {
  
 
   return (
     <>
-      <h1>Helo</h1>
+      <main>
+        <div>
+          
+        </div>
+      </main>
     </>
   )
 }
