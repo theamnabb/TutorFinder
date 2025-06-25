ef2c2085d60beb1b475daf61e2f15ca3d48450e3
Amna BB <theamnabb@gmail.com>
2025-06-25 15:09:31 +0500

    admincontext

---
+++ admin/src/context/AdminContext.jsx
@@ -0,0 +1,19 @@
+import { useContext, useState } from "react"
+
+// create context
+export const AdminContext = useContext();
+
+const AdminContextProvider = (props) => {
+const [atoken, setAtoken] = useState('dummy token') ;
+const value = {
+
+}
+
+  return (
+    <AdminContext.Provider value={value}>
+        {props.children}
+    </AdminContext.Provider>
+  )
+}
+
+export default AdminContextProvider
