286c67e43fa93146d4daf9414061ff04ec7f2bda
Amna BB <theamnabb@gmail.com>
2025-06-26 14:13:19 +0500

    upadted admin pages

--- admin/src/pages/admin/AllSessions.jsx
+++ admin/src/pages/admin/AllSessions.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+// dummy Data for Sessions // Please replace it 
 
 const initialSessions = [
   {
--- admin/src/pages/admin/Dashboard.jsx
+++ admin/src/pages/admin/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import { Users, Calendar, AlertCircle, MessageSquare } from "lucide-react";
 
 const Dashboard = () => {
